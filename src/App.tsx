595242e158aad883df65a914ed1e702ae6389645
gabsdv <gs44@illinois.edu>
2025-10-05 20:40:42 -0500

    Layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Search from './pages/Search';
+import Gallery from './pages/Gallery';
+import ArtPiece from './pages/ArtPiece';
+
+import Layout from './components/Layout';
+
+
+
+export default function App() {
+    return (
+        <BrowserRouter basename='/mp2'>
+            <Routes>
+                <Route path='/' element={<Layout />}>
+                    <Route index element={<Search />} />
+                    <Route path='piece/:id' element={<ArtPiece />} />
+                    <Route path='gallery' element={<Gallery />} />
+                    <Route path='search' element={<Search />} />
+                    <Route index element={<Search />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
-
-export default App;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,38 @@
+import { Outlet, Link } from 'react-router-dom';
+
+import styles from '../styles/Layout.module.css';
+
+
+
+export default function Layout() {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Art Search Directory</h1>
+
+            <nav className={styles.navContainer}>
+                <LinkElement
+                    route='/search'
+                    title='Search'
+                />
+
+                <LinkElement
+                    route='/gallery'
+                    title='Gallery'
+                />
+            </nav>
+
+            <Outlet />
+        </div>
+    );
+}
+
+
+
+interface LinkElementProps {
+    route: string;
+    title: string;
+}
+
+function LinkElement({ route, title }: LinkElementProps) {
+    return <Link to={route} className={styles.navItem}>{title}</Link>;
+}
